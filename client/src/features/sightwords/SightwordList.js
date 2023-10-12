190be330afb9f6044c40dbd2c57f93bdf1b0b036
Edwin <mredwinrodriguez@gmail.com>
2023-10-12 09:44:00 -0400

    track score

--- client/src/features/sightwords/SightwordList.js
+++ client/src/features/sightwords/SightwordList.js
@@ -1,27 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import {Container, Col, Row, Button} from 'reactstrap';
 import Sightword from './Sightword';
 
 const SightwordList = () => {
 	const [showMic, toggleMic] = useState(true);
 	const [currentWord, updateCurrentWord] = useState(null);
-	const words = ['it', "black", "red", "into", "in", "white", "brown"]
+	const [score, updateScore] = useState(0)
+
+	const wordsFullArray = ['it', "black", "red", "into", "in", "white", "brown"];
+	const words = useRef(['it', "black", "red", "into", "in", "white", "brown"]);
 
 	const onRead = () => {
-		const randomNumber = Math.floor(Math.random() * words.length);
-		const toSay = words.splice(randomNumber, 1);
+		const randomNumber = Math.floor(Math.random() * words.current.length);
+		const toSay = words.current.splice(randomNumber, 1);
 		updateCurrentWord(toSay[0])
-		console.log(words)
-		if (words.length === 0) toggleMic(false);
-		// const utter = new window.SpeechSynthesisUtterance(toSay[0]);
-		// window.speechSynthesis.speak(utter);
+		if (words.current.length === 0) toggleMic(false);
+		const utter = new window.SpeechSynthesisUtterance(toSay[0]);
+		window.speechSynthesis.speak(utter); 
 	};
 
 	const compare = (selection) => {
-		if (selection === currentWord) {
+		if (!currentWord) {
+			const utter = new window.SpeechSynthesisUtterance("Select a new word");
+			window.speechSynthesis.speak(utter); 
+		} else if (selection === currentWord) {
+			updateScore(score + 1);
 			const utter = new window.SpeechSynthesisUtterance("Great Job");
 			window.speechSynthesis.speak(utter);
+			updateCurrentWord(null)
 		} else {
+			if (score > 0) updateScore(score - 1);
 			const utter = new window.SpeechSynthesisUtterance("Try Again");
 			window.speechSynthesis.speak(utter);
 		}
@@ -29,9 +37,11 @@ const SightwordList = () => {
 
 	return (
 		<Container>
+			<h1>Score</h1>
+			<div>{score}</div>
 			{showMic ? <Button onClick={onRead}><i className="fa fa-microphone" aria-hidden="true"></i></Button> : ""}
 			<Row>
-				{words.map((word, idx) => {
+				{wordsFullArray.map((word, idx) => {
 					return (
 						<Col className='m-3 col-sm-4' key={idx}>
 							<Sightword word={word} compare={compare} />
