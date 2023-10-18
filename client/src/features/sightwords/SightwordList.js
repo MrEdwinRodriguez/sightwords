15ac75a9293abae01998e6cb83f0964dd98234c6
Edwin <mredwinrodriguez@gmail.com>
2023-10-18 16:41:13 -0400

    confetti and alert

--- client/src/features/sightwords/SightwordList.js
+++ client/src/features/sightwords/SightwordList.js
@@ -1,19 +1,24 @@
 import React, {useRef, useEffect, useState} from 'react';
-import {Container, Col, Row, Button} from 'reactstrap';
+import {Container, Col, Row, Button, Alert} from 'reactstrap';
 import Sightword from './Sightword';
 import AnimatedWord from './AnimatedWord';
 import DropdownComponent from '../../components/DropdownComponent';
 import { getEncourangmentPhrase } from '../../helpers/encourage';
 import { sightwordHint } from '../../helpers/hint';
+import ConfettiExplosion from 'react-confetti-explosion';
 import { getAllSightWords, selectSighwordListsById, getAllUnits } from './sightwordSlice'
 
 const SightwordList = () => {
-	const [showMic, toggleMic] = useState(true);
+	const [showMic, toggleMic] = useState(false);
+	const [showConfetti, toggleConfetti] = useState(false);
 	const [showRepeat, toggleRepeat] = useState(false);
 	const [currentWord, updateCurrentWord] = useState(null);
 	const [unit, updateUnit] = useState('Choose Unit');
 	const [wordsFullArray, updateWordFullsArray] = useState([]);
 	const [score, updateScore] = useState(0);
+	const [alert, toggleAlert] = useState(false);
+	const [alertText, updateAlertText] = useState("");
+	const [scorePercentage, updateScorePercentage] = useState(0);
 
 	const allUnits = getAllUnits();
 	let words = useRef([]);
@@ -51,8 +56,18 @@ const SightwordList = () => {
 			window.speechSynthesis.speak(utter);
 			updateCurrentWord(null)
 			if (words.current.length === 0) {
+				updateScorePercentage((score+1)/wordsFullArray.length);
+				toggleConfetti(true);
+				toggleAlert(true);
 				toggleMic(false);
 				toggleRepeat(false);
+				if ((score+1)/wordsFullArray.length === 1) {
+					updateAlertText(`Perfect Score! You got ${score+1} points.`)
+				} else if ((score+1)/wordsFullArray.length > .6) {
+					updateAlertText(`Good Job! You got ${score+1} points.`)
+				} else {
+					updateAlertText(`Let's try this one again. You got ${score+1} points.`)
+				}
 			} else {
 				toggleMic(true);
 				toggleRepeat(false);
@@ -65,15 +80,27 @@ const SightwordList = () => {
 		}
 	};
 
+	const done = () => {
+
+	};
+
 	const handleDropDown = (id) => {
 		const unitSelected = selectSighwordListsById(id);
 		words.current = [...unitSelected.words];
 		updateWordFullsArray(unitSelected.words);
-		updateUnit(unitSelected.unit)	
+		updateUnit(unitSelected.unit)
+		toggleMic(true);
+		updateScore(0);
+	};
+
+	const hideAlert = () => {
+		toggleAlert(false);
 	};
 
 	return (
 		<Container className="margin-top-20">
+		{alert && <Alert color={scorePercentage > .6 ? "primary" : "warning"}>{alertText}<span className="fr" onClick={hideAlert}>x</span></Alert>}
+		{showConfetti && <ConfettiExplosion particleCount={200} duration={4000} width={1000}/>}
 			<DropdownComponent title="Choose Unit" options={allUnits} callback={handleDropDown}/>
 			{unit && unit !== "Choose Unit" && <h1>Unit {unit}</h1>}
 			<h3>Score</h3>
