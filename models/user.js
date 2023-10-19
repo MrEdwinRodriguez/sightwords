e16b9ed557dde61bf72f01ee9b2a21547480b56f
Edwin <mredwinrodriguez@gmail.com>
2023-10-19 09:58:50 -0400

    adding models

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+let UserSchema = new Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true }, //, required: true
+    // country: { type: Number, ref: 'Country' },
+    status: {type: String, enum: ['Inactive', 'Active'], default: "Active"},
+    is_admin: { type: Boolean, default: false },
+    reset_password_token: String,
+    reset_password_expires: Date,
+    last_login:  { type: Date, default: Date.now },
+  }, {
+    timestamps: true
+  });
+
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", UserSchema);
