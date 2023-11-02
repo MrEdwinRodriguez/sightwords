6f0a998ff6d21aedea5f2a3b5602c74fa2bc0903
Edwin <mredwinrodriguez@gmail.com>
2023-11-02 18:24:03 -0400

    backend - authentication

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const mongoose = require('mongoose');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to server'),
+    err => console.log(err)
+);
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,8 +33,10 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- models/user.js
+++ models/user.js
@@ -3,12 +3,13 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 let UserSchema = new Schema({
-    name: { type: String, required: true },
-    username: { type: String, required: true }, //, required: true
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    // username: { type: String, required: true }, //, required: true
     status: {type: String, enum: ['Inactive', 'Active'], default: "Active"},
     is_admin: { type: Boolean, default: false },
-    reset_password_token: String,
-    reset_password_expires: Date,
+    // reset_password_token: String,
+    // reset_password_expires: Date,
     last_login:  { type: Date, default: Date.now },
   }, {
     timestamps: true
